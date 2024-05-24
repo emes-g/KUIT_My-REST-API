854252122678874e09e23cb93e45fa433438c24c
Minseok <saltishDev@gmail.com>
2024-05-24 11:47:14 +0900

    Feat: Bean Validation 추가

--- src/main/java/kuit/server/dto/user/PatchPhoneNumberRequest.java
+++ src/main/java/kuit/server/dto/user/PatchPhoneNumberRequest.java
@@ -1,16 +1,19 @@
 package kuit.server.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class PatchPhoneNumberRequest {
 
-    @NotNull(message = "phoneNumber: {NotNull}")
+    @Length(max = 13, message = "최대 {max}자리까지 가능합니다")
+    @NotBlank(message = "전화번호를 입력해주세요")
     private String phoneNumber;
 
 }
